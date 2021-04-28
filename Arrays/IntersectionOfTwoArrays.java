4e211149f9ad2e0e446bee1033bf4c4924cf742b
rlt23 <rlt23@njit.edu>
2021-04-28 14:40:40 -0400

    Create IntersectionOfTwoArrays.java

---
+++ Arrays/IntersectionOfTwoArrays.java
@@ -0,0 +1,29 @@
+//350. Intersection of Two Arrays II
+//Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order./
+
+//HASHMAP SOLUTION
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+     if(nums1.length == 0 || nums1 == null)return new int[0];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        ArrayList<Integer> li = new ArrayList<>();
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1>n2)return intersect(nums2,nums1);
+        for(int num:nums1){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int num:nums2){
+            if(map.containsKey(num)){
+                li.add(num);
+                map.put(num, map.get(num)-1);
+                map.remove(num,0);
+            }
+        }
+        int [] result = new int[li.size()];
+        for(int i=0;i<li.size();i++){
+            result[i] = li.get(i);
+        }
+        return result;
+    }
+}
