fa00381a1fbf13cc53d69c68a305e5e2ab71a328
rlt23 <rlt23@njit.edu>
2021-05-07 14:13:20 -0400

    Update IntersectionOfTwoArrays.java

--- Arrays/IntersectionOfTwoArrays.java
+++ Arrays/IntersectionOfTwoArrays.java
@@ -27,3 +27,36 @@ class Solution {
         return result;
     }
 }
+
+//Two Pointer Solution
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+     if(nums1.length == 0 || nums1 == null)return new int[0];
+       // HashMap<Integer,Integer> map = new HashMap<>();
+        ArrayList<Integer> li = new ArrayList<>();
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        if(n1>n2)return intersect(nums2,nums1);
+        int ptr1 = 0;
+        int ptr2 =0;
+        while(ptr1<n1 && ptr2 < n2){
+            if(nums1[ptr1] == nums2[ptr2]){
+                li.add(nums1[ptr1]);
+                ptr1++;
+                ptr2++;
+            }else if(nums1[ptr1]>nums2[ptr2]){
+                ptr2++;
+            }else{
+                ptr1++;
+            }
+        }
+        int [] result = new int[li.size()];
+        for(int i=0;i<li.size();i++){
+            result[i] = li.get(i);
+        }
+        return result;
+    }
+}
